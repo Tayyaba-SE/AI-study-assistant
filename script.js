2399ce81a9d248a3ae5c1cddcac1ac3910f66be5
TAYYABA NISA <nisatayyaba3@gmail.com>
2026-07-29 21:58:11 +0500

    Implement DOM selection and capture user input

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const input = document.querySelector(".question-input");
 const button = document.querySelector(".ask-btn");
 const response = document.querySelector(".response");
+
+button.addEventListener("click", function(){
+
+    response.innerHTML +=
+    `<div class="user-message">
+        ${input.value}
+    </div>`;
+
+});
